a725d99439bcc3440eb9ec785df1a0f62b890b1d
audrastump <71152145+audrastump@users.noreply.github.com>
2022-05-01 14:30:50 -0500

    fixed copy command tests - 69/77 tests pass

--- SharedCode/CopyCommand.cpp
+++ SharedCode/CopyCommand.cpp
@@ -4,34 +4,35 @@ using namespace std;
 
 CopyCommand::CopyCommand(AbstractFileSystem* files) : fileSys(files) {}
 
-//use prototype pattern because objects should be in charge of creating their own copies (especially if I
-// can't get private member variables)
-int CopyCommand::execute(std::string commandName) {
-
-	string name;
-	string tag;
-	istringstream iss(commandName);
-	//extracting the name and tag 
-	iss >> name;
-	iss >> tag;
-	set<string> fileNames = fileSys->getFileNames();
-	set<string>::iterator it = fileNames.find(name);
-	//if the command name cannot be found in the file system we will return an invalid file
-	if (it == fileNames.end()) {
-		cout << "Could not find command name" << endl;
-		return invalidFile;
+int CopyCommand::execute(std::string command) {
+	//extracting the file name and the new name
+	string firstName;
+	string newName;
+	istringstream iss(command);
+	iss >> firstName;
+	iss >> newName;
+	set<string> fileSet;
+	fileSet = fileSys->getFileNames();
+	if (fileSet.find(firstName) == fileSet.end()) {
+		return copyCommandError;
 	}
-	
-	AbstractFile* curFile = fileSys->openFile(name);
-	fileSys->closeFile(curFile);
-
-	AbstractFile* newFile = curFile->clone(tag);
-	if (fileSys->addFile(newFile->getName(), newFile) == successful) {
-		return copyCommandSuccess;
+	//trying to find if there is another file of that new name
+	string fullname = newName + firstName.substr(firstName.find('.'));
+	if (fileSet.find(fullname) == fileSet.end()) {
+		//copy the files
+		AbstractFile* file = fileSys->openFile(firstName);
+		AbstractFile* copiedFile = file->clone(fullname);
+		auto newFileName = copiedFile->getName();
+		auto ret = fileSys->addFile(newFileName, copiedFile);
+		fileSys->closeFile(file);
+		return ret;
 	}
-	delete newFile;
-	return copyCommandError;
+	else {
+		return copyCommandError;
+	}
 }
+
+
 
 void CopyCommand::displayInfo() {
 	cout << "To copy files, can be invoked with the command: cp <file_to_copy> <new_name_with_no_extension>" << endl;
